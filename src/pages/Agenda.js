15c97711ea9b324e4a077c1eb6673a9b6592fddb
KELAI Afaf <ea_kelai@esi.dz>
2019-10-07 20:26:59 +0100

    problem google icons

--- src/pages/Agenda.js
+++ src/pages/Agenda.js
@@ -18,6 +18,24 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 
+import styled from 'styled-components'
+import Table from '../components/Agenda/Table'
+
+
+
+
+const StyledImg = styled.div`
+    width: 14%;
+    height:68px;
+    margin-top:10px;
+    img{
+      width:37%;
+    }
+    
+`;
+
+
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
@@ -97,8 +115,12 @@ export default function Agenda() {
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
+
+        
       >
-        <Toolbar>
+        <Toolbar  style={{backgroundColor: '#555555' }}  >
+            
+
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -108,12 +130,19 @@ export default function Agenda() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap>
-            Persistent drawer
+            
+
+          <StyledImg>
+            <img src={require("../images/calendar.svg")}></img>
+            </StyledImg>
+          <Typography style={{fontSize:'56px',color:'#E2007A'}}variant="h6" noWrap>
+            Agenda
           </Typography>
+
+          
         </Toolbar>
       </AppBar>
-      <Drawer
+      <Drawer  
         className={classes.drawer}
         variant="persistent"
         anchor="left"
@@ -152,29 +181,7 @@ export default function Agenda() {
         })}
       >
         <div className={classes.drawerHeader} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Table></Table>
       </main>
     </div>
   );
